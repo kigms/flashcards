d0b1db56dc75b6a06065fb5be8704c5e4ff6da62
kigms <kigms@protonmail.com>
2025-03-06 00:47:12 -0600

    Modified App, added Card component, and started prop functionality

---
+++ src/Card.jsx
@@ -0,0 +1,14 @@
+
+
+const Card = ({text}) => {
+    //Handle click function?
+
+    return (
+        // onClick event?
+        <button className='Card'>
+            <p className={text}></p>
+        </button>
+    );
+}
+
+export default Card();
